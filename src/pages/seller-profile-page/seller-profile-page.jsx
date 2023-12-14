0f25afa3c824c7b6021f950f817be37f749878fa
denniztr <denniztr@gmail.com>
2023-12-14 17:37:10 +0300

    seller-profile-page html changes

--- src/pages/seller-profile-page/seller-profile-page.jsx
+++ src/pages/seller-profile-page/seller-profile-page.jsx
@@ -8,12 +8,14 @@ export const SellerProfilePage = () => {
     <div className='wrapper'>
       <div className='container'>
         <main className='main'>
-          <div className='main__center-block'>
-            <Menu/>
-            <SellerProfileData />
-          </div>
-          <div className='main__content'>
-            <ContentCards/>
+          <div className='main__container'>
+            <div className='main__center-block'>
+              <Menu/>
+              <SellerProfileData />
+            </div>
+            <div className='main__content'>
+              <ContentCards/>
+            </div>
           </div>
         </main>
       </div>
